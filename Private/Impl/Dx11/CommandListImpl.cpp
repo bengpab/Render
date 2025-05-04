7e50d7d6052546c56e0994cd4e899cee19f5ebf1
Tom Pyne <tompyne@live.co.uk>
2025-05-04 22:36:30 +0100

    [Raytracing] Adding fix for last commit

--- Private/Impl/Dx11/CommandListImpl.cpp
+++ Private/Impl/Dx11/CommandListImpl.cpp
@@ -254,7 +254,7 @@ void CommandList::DispatchMesh(uint32_t x, uint32_t y, uint32_t z) { assert(0); 
 
 void CommandList::BuildRaytracingScene(RaytracingScene_t scene) { assert(0); }
 
-void CommandList::DispatchRays() { assert(0); }
+void CommandList::DispatchRays(uint32_t x, uint32_t y, uint32_t z) { assert(0); }
 
 // Dx11 Style Bind Commands
 void CommandList::BindVertexSRVs(uint32_t startSlot, uint32_t count, const ShaderResourceView_t* const srvs)
